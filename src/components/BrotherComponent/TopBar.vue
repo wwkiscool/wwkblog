5ad6f008aafdd127e907db7d1c6705ec3478e719
wwk <1198949718@qq.com>
2022-08-01 17:59:15 +0800

    配置相关2022/8/1

---
+++ src/components/BrotherComponent/TopBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div class="PcTopBar">
+        top
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/test/index.vue
@@ -0,0 +1,17 @@
+
+<template>
+<div>{{content}}</div>
+</template>
+<script setup>
+
+import { ref,reactive,toRefs } from "vue";
+const data = reactive({
+    name:"wwk",
+    age:25
+})
+
+const content = ref('wwk');
+const {name,age}  = toRefs(data);
+</script>
+<style scoped lang="less">
+</style>
